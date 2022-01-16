7de7ec5f03ccb87a05862cd0045c51b1e9800824
xkjdox <85050968+xkjdox@users.noreply.github.com>
2022-01-16 12:32:06 +0800

    Create cek-ip.sh

---
+++ cek-ip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/xkjdox/rijrekeksdpdolwoqkqkakodix/main/Nxbdjekkwso.txt | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
+clear
